8e2d0eb06c2a91e1768c595ea40a95dc6af213df
Herob527 <szymon.wrzos@onet.pl>
2025-04-17 23:39:34 +0200

    Moved to nuxt tailwdincss since it seems to work (LSP works)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,18 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from 'nuxt/config';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
   modules: [
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/test-utils",
-    "@nuxt/ui",
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
   ],
-  "plugins": ["~/plugins/db-service-provider.ts"],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
+  plugins: ['~/plugins/db-service-provider.ts'],
 });
