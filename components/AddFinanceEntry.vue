fb300fbfcd46ab477555c982fec6406446cfd11a
Herob527 <szymon.wrzos@onet.pl>
2025-04-17 23:38:53 +0200

    Checking out

--- components/AddFinanceEntry.vue
+++ components/AddFinanceEntry.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts"></script>
+
 <template>
-  <div>
+  <div class="mt-2 border-2 border-gray-500 inline-flex flex-col">
     <h2>Container</h2>
+    <input type="text" />
   </div>
 </template>
