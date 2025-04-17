85bc02fd4287fe3942e50b3aa68b01a070f604a6
Herob527 <szymon.wrzos@onet.pl>
2025-04-17 23:58:26 +0200

    Revert "Moved to nuxt tailwdincss since it seems to work (LSP works)"
    This reverts commit 8e2d0eb06c2a91e1768c595ea40a95dc6af213df.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,24 @@
-import { defineNuxtConfig } from 'nuxt/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxtjs/tailwindcss',
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
   ],
-  plugins: ['~/plugins/db-service-provider.ts'],
+  "plugins": ["~/plugins/db-service-provider.ts"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 });
