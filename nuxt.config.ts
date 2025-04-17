cd5ada26ba4b8fd7f1ba193cda4a100aba1e5a5d
Herob527 <szymon.wrzos@onet.pl>
2025-04-17 11:57:58 +0200

    Added tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
   modules: [
     "@nuxt/eslint",
     "@nuxt/fonts",
@@ -12,4 +14,9 @@ export default defineNuxtConfig({
     "@nuxt/test-utils",
     "@nuxt/ui",
   ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 });
