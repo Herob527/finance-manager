377a8cd25f9b43222b3e0ffd38b3de51db876e3e
Herob527 <szymon.wrzos@onet.pl>
2025-04-21 20:19:35 +0200

    Added form (potentially working)

--- components/AddFinanceEntry.vue
+++ components/AddFinanceEntry.vue
@@ -1,29 +1,71 @@
 <script setup lang="ts">
-const CATEGORIES = [
-  'Groceries',
-  'Utilities',
-  'Entertainment',
-  'Other',
-  'Job',
-].map((c) => ({ value: c.toLowerCase(), label: c }));
+import { type } from 'arktype';
+import { shallowReactive } from 'vue';
+import UForm from '@nuxt/ui/components/Form.vue';
+import UFormField from '@nuxt/ui/components/FormField.vue';
+import UCalendar from '@nuxt/ui/components/Calendar.vue';
+import type { FormSubmitEvent } from '@nuxt/ui';
+import { CalendarDate } from '@internationalized/date';
+
+const emit = defineEmits<{
+  (e: 'submit', data: Schema): void;
+}>();
+
+const CATEGORIES = (
+  ['Groceries', 'Utilities', 'Entertainment', 'Other', 'Job'] as const
+).map((c) => ({ value: c.toLowerCase() as Lowercase<typeof c>, label: c }));
+
+const schema = type({
+  amount: type.number.configure({
+    message: (ctx) => {
+      return ctx.code === 'domain' ? 'Amount is required' : '';
+    },
+  }),
+  description: type.string.moreThanLength(3).or('undefined'),
+  category: type.enumerated(...CATEGORIES.map((c) => c.value)).configure({
+    message: (ctx) =>
+      ctx.code === 'predicate' ? 'Category must be picked' : '',
+  }),
+  date: type.instanceOf(CalendarDate).configure({
+    message: (ctx) => (ctx.code === 'proto' ? 'Date is required' : ''),
+  }),
+});
+type Schema = typeof schema.infer;
+
+const state = shallowReactive<Partial<Schema>>({
+  date: undefined,
+  amount: undefined,
+  category: undefined,
+  description: undefined,
+});
+
+const handleSubmit = (ev: FormSubmitEvent<Schema>) => {
+  emit('submit', ev.data);
+};
 </script>
 
 <template>
-  <UForm class="space-y-4">
+  <UForm
+    :schema="schema"
+    :state="state"
+    class="space-y-4 border-primary-500 border-2 p-2 rounded-2xl"
+    @submit="handleSubmit"
+  >
+    <span class="font-bold text-2xl mb-4"> Fill the data </span>
     <UFormField label="Amount" name="amount">
-      <UInputNumber />
+      <UInputNumber v-model="state.amount" />
     </UFormField>
 
     <UFormField label="Description" name="description">
-      <UInput />
+      <UInput v-model="state.description" />
     </UFormField>
 
     <UFormField label="Category" name="category">
-      <USelect :items="CATEGORIES" class="w-48" />
+      <USelect v-model="state.category" :items="CATEGORIES" class="w-48" />
     </UFormField>
 
     <UFormField label="Date" name="date">
-      <UCalendar />
+      <UCalendar v-model="state.date" />
     </UFormField>
 
     <UButton type="submit"> Submit </UButton>
