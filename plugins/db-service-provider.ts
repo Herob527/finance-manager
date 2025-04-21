2ff89338ad681a002b4283411eb2febd135b584c
Herob527 <szymon.wrzos@onet.pl>
2025-04-21 20:19:43 +0200

    Renaming adjustments

--- plugins/db-service-provider.ts
+++ plugins/db-service-provider.ts
@@ -1,11 +1,11 @@
-import { defineNuxtPlugin } from "nuxt/app";
-import IndexedDBStorage from "../src/services/LocalDatabase";
-import LoggingStorage from "../src/services/DummyServer";
+import { defineNuxtPlugin } from 'nuxt/app';
+import IndexedDBStorage from '../src/services/LocalDatabase';
+import LoggingStorage from '../src/services/DummyFinanceService';
 
 export default defineNuxtPlugin({
   name: 'db-service-provider',
   setup(nuxtApp) {
-    const StorageClass = import.meta.server ? LoggingStorage : IndexedDBStorage
-    nuxtApp.provide('storage', new StorageClass())
+    const StorageClass = import.meta.server ? LoggingStorage : IndexedDBStorage;
+    nuxtApp.provide('storage', new StorageClass());
   },
 });
