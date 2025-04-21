9aae590d6e8437bf4aa0b359af3d3fedc37e91b2
Herob527 <szymon.wrzos@onet.pl>
2025-04-21 20:31:44 +0200

    Finally added pushing data to DB

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,46 @@
 <script setup lang="ts">
-import AddFinanceEntry from '../components/AddFinanceEntry.vue';
+import { liveQuery } from 'dexie';
+import AddFinanceEntry, {
+  type Schema,
+} from '../components/AddFinanceEntry.vue';
 import { useDatabase } from '../composable/use-database';
+import { from } from 'rxjs';
+import { useObservable } from '@vueuse/rxjs';
+import { today, getLocalTimeZone } from '@internationalized/date';
 
 const test = useDatabase();
+
+const data = useObservable(
+  from(
+    liveQuery(async () => {
+      return await test.getAll();
+    }),
+  ),
+);
+
+const handleSubmit = (data: Schema) => {
+  test.add({
+    date: data.date.toDate(getLocalTimeZone()),
+    description: data.description,
+    amount: data.amount,
+    category: data.category,
+    enabled: false,
+  });
+};
 </script>
 
 <template>
   <main class="container mx-auto flex flex-col">
-    <span class="text-3xl">Test</span>
-    <AddFinanceEntry />
+    <div class="flex flex-row gap-4 mt-4">
+      <AddFinanceEntry @submit="handleSubmit" />
+      <div>
+        <template v-if="data && data?.length > 0">
+          <span v-for="entry in data ?? []" :key="entry.id">
+            {{ entry.category }}
+          </span>
+        </template>
+        <span v-else> No data yet </span>
+      </div>
+    </div>
   </main>
 </template>
