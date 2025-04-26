cee389a6d3f328e056dee503cd2ed1f6c043aee9
Herob527 <szymon.wrzos@onet.pl>
2025-04-26 22:16:38 +0200

    Added Health category

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const CATEGORIES = (
-  ['Groceries', 'Utilities', 'Entertainment', 'Other', 'Job'] as const
+  ['Groceries', 'Health', 'Utilities', 'Entertainment', 'Other', 'Job'] as const
 ).map((c) => ({ value: c.toLowerCase() as Lowercase<typeof c>, label: c }));
