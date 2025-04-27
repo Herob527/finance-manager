faf03fc7510d75d64b9da5d97f71540e578930c5
Herob527 <szymon.wrzos@onet.pl>
2025-04-27 23:39:11 +0200

    Working on pagination

--- components/DisplayFinanceEntries.vue
+++ components/DisplayFinanceEntries.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { UButton, UCheckbox, UTable } from '#components';
+import { getPaginationRowModel } from '@tanstack/vue-table';
 import type { TableColumn } from '@nuxt/ui';
-import { computed, h } from 'vue';
+import { computed, h, type VNodeRef } from 'vue';
 import { CATEGORIES } from '~/src/constants';
 import type { FinanceEntry } from '~/src/types/FinanceEntry';
 
@@ -76,13 +77,35 @@ const columns = computed(
       },
     ] as const satisfies TableColumn<FinanceEntry>[],
 );
+const table = ref();
+const pagination = ref({
+  pageIndex: 0,
+  pageSize: 10,
+});
 </script>
 <template>
-  <UTable
-    :data="error ? [] : data"
-    :empty="error ? 'Error loading data' : 'No data yet'"
-    :columns="columns"
-    :loading="loading || error"
-    :loading-color="error ? 'error' : 'primary'"
-  />
+  <div class="flex flex-col">
+    <UTable
+      :ref="table"
+      v-model:pagination="pagination"
+      :data="error ? [] : data"
+      :empty="error ? 'Error loading data' : 'No data yet'"
+      :columns="columns"
+      :pagination-options="{
+        getPaginationRowModel: getPaginationRowModel(),
+      }"
+      :loading="loading || error"
+      :loading-color="error ? 'error' : 'primary'"
+    />
+    <div class="flex justify-center border-t border-default pt-4">
+      <UPagination
+        :default-page="
+          (table?.tableApi?.getState().pagination.pageIndex || 0) + 1
+        "
+        :items-per-page="table?.tableApi?.getState().pagination.pageSize"
+        :total="table?.tableApi?.getFilteredRowModel().rows.length"
+        @update:page="(p) => table?.tableApi?.setPageIndex(p - 1)"
+      />
+    </div>
+  </div>
 </template>
