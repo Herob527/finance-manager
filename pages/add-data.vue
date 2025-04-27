a10d1972c46b01cce4a6460612ea464137d16159
Herob527 <szymon.wrzos@onet.pl>
2025-04-27 23:39:17 +0200

    Added preview for data in adding section

--- pages/add-data.vue
+++ pages/add-data.vue
@@ -3,6 +3,10 @@ import { getLocalTimeZone } from '@internationalized/date';
 import { AddFinanceEntry } from '#components';
 import type { Schema } from '~/components/AddFinanceEntry.vue';
 import { useDatabase } from '#imports';
+import { useObservable } from '@vueuse/rxjs';
+import { liveQuery } from 'dexie';
+import { from, map, catchError, of } from 'rxjs';
+import type { FinanceEntry } from '~/src/types/FinanceEntry';
 
 const test = useDatabase();
 
@@ -15,6 +19,47 @@ const handleSubmit = (data: Schema) => {
     enabled: true,
   });
 };
+
+function createDataState<T>() {
+  return {
+    data: [] as T,
+    isLoading: true,
+    error: null as unknown | null,
+  };
+}
+const data$ = useObservable(
+  from(liveQuery(async () => await test.getAll())).pipe(
+    map((result) => ({
+      data: result,
+      isLoading: false,
+      error: null,
+    })),
+    catchError((error) =>
+      of({
+        data: [],
+        isLoading: false,
+        error,
+      }),
+    ),
+  ),
+  {
+    initialValue: createDataState<FinanceEntry[]>(),
+  },
+);
+
+const processedData = computed(() => {
+  return (data$.value.data ?? []).toSorted(
+    (a, b) => a.date.getTime() - b.date.getTime(),
+  );
+});
+
+const handleToggle = async ({ id }: { id: number }) => {
+  await test.toggle(id);
+};
+
+const handleRemove = async ({ id }: { id: number }) => {
+  await test.delete(id);
+};
 </script>
 
 <template>
@@ -22,6 +67,21 @@ const handleSubmit = (data: Schema) => {
     <h1 class="text-4xl font-bold my-12">Finance Tracker</h1>
     <div class="flex flex-row gap-4">
       <AddFinanceEntry @submit="handleSubmit" />
+      <DisplayFinanceEntries
+        :class="[
+          'border-2',
+          {
+            'border-primary-500': !data$.error,
+            'border-error-500': !!data$.error,
+          },
+          'rounded-2xl',
+        ]"
+        :data="processedData"
+        :loading="data$.isLoading"
+        :error="!!data$.error"
+        @toggle="(id) => handleToggle({ id })"
+        @delete="(id) => handleRemove({ id })"
+      />
     </div>
   </main>
 </template>
