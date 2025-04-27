6b172efc8cfbb2edf058b4a5271539b179b5ecff
Herob527 <szymon.wrzos@onet.pl>
2025-04-27 15:45:10 +0200

    Moved views to separate subpage

--- pages/add-data.vue
+++ pages/add-data.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+import { getLocalTimeZone } from '@internationalized/date';
+import { AddFinanceEntry } from '#components';
+import type { Schema } from '~/components/AddFinanceEntry.vue';
+import { useDatabase } from '#imports';
+
+const test = useDatabase();
+
+const handleSubmit = (data: Schema) => {
+  test.add({
+    date: data.date.toDate(getLocalTimeZone()),
+    description: data.description,
+    amount: data.amount,
+    category: data.category,
+    enabled: true,
+  });
+};
+</script>
+
 <template>
-  <p>Add</p>
+  <main class="container mx-auto flex flex-col">
+    <h1 class="text-4xl font-bold my-12">Finance Tracker</h1>
+    <div class="flex flex-row gap-4 mt-4">
+      <AddFinanceEntry @submit="handleSubmit" />
+    </div>
+  </main>
 </template>
--- pages/display-data.vue
+++ pages/display-data.vue
@@ -1,3 +1,49 @@
+<script setup lang="ts">
+import { liveQuery } from 'dexie';
+import { from } from 'rxjs';
+import { useObservable } from '@vueuse/rxjs';
+import { ChartDisplay, DisplayFinanceEntries } from '#components';
+import { useDatabase } from '#imports';
+
+const test = useDatabase();
+
+const data = useObservable(
+  from(
+    liveQuery(async () => {
+      return await test.getAll();
+    }),
+  ),
+);
+
+const handleToggle = async ({ id }: { id: number }) => {
+  await test.toggle(id);
+};
+
+const handleRemove = async ({ id }: { id: number }) => {
+  await test.delete(id);
+};
+
+const processedData = computed(() => {
+  return (data.value ?? []).toSorted(
+    (a, b) => a.date.getTime() - b.date.getTime(),
+  );
+});
+</script>
+
 <template>
-  <p>Display</p>
+  <main class="container mx-auto flex flex-col">
+    <h1 class="text-4xl font-bold my-12">Finance Tracker</h1>
+    <template v-if="data && data?.length > 0">
+      <DisplayFinanceEntries
+        class="border-2 border-primary-500 rounded-2xl"
+        :data="processedData"
+        @toggle="(id) => handleToggle({ id })"
+        @delete="(id) => handleRemove({ id })"
+      />
+    </template>
+    <span v-else> No data yet </span>
+    <div class="mt-4">
+      <ChartDisplay :data="processedData.filter(({ enabled }) => enabled)" />
+    </div>
+  </main>
 </template>
