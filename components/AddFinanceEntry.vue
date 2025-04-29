1ef0dcf66f77eec12701f180889e7fbe6d2c2f75
Herob527 <szymon.wrzos@onet.pl>
2025-04-29 11:28:00 +0200

    Working on tabs

--- components/AddFinanceEntry.vue
+++ components/AddFinanceEntry.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { type } from 'arktype';
-import { shallowReactive } from 'vue';
-import { CalendarDate } from '@internationalized/date';
 import {
   UCalendar,
   UForm,
@@ -10,8 +7,11 @@ import {
   UInputNumber,
   USelect,
 } from '#components';
+import { CalendarDate } from '@internationalized/date';
+import type { FormSubmitEvent, TabsItem } from '@nuxt/ui';
+import { type } from 'arktype';
+import { shallowReactive } from 'vue';
 import { CATEGORIES } from '~/src/constants';
-import type { FormSubmitEvent } from '@nuxt/ui';
 
 const emit = defineEmits<{
   (e: 'submit', data: Schema): void;
@@ -50,39 +50,53 @@ const handleSubmit = (ev: FormSubmitEvent<Schema>) => {
   state.category = undefined;
   state.description = undefined;
 };
+
+const items = ref<TabsItem[]>([
+  {
+    label: 'Occurrence',
+    icon: 'i-lucide-user',
+  },
+  {
+    label: 'Series',
+    icon: 'i-lucide-lock',
+  },
+]);
 </script>
 
 <template>
-  <UForm
-    :schema="schema"
-    :state="state"
-    class="flex flex-row gap-4 border-primary-500 border-2 p-4 rounded-2xl h-min"
-    @submit="handleSubmit"
-  >
-    <div class="flex flex-col gap-4">
-      <span class="font-bold text-2xl mb-4"> Fill the data </span>
-      <UFormField label="Amount" name="amount">
-        <UInputNumber
-          v-model="state.amount"
-          :step="0.01"
-          :step-snapping="false"
-        />
-      </UFormField>
+  <section>
+    <UTabs :items="items" class="w-full" />
+    <UForm
+      :schema="schema"
+      :state="state"
+      class="flex flex-row gap-4 border-primary-500 border-2 p-4 rounded-2xl h-min"
+      @submit="handleSubmit"
+    >
+      <div class="flex flex-col gap-4">
+        <span class="font-bold text-2xl mb-4"> Fill the data </span>
+        <UFormField label="Amount" name="amount">
+          <UInputNumber
+            v-model="state.amount"
+            :step="0.01"
+            :step-snapping="false"
+          />
+        </UFormField>
 
-      <UFormField label="Description" name="description">
-        <UInput v-model="state.description" />
-      </UFormField>
+        <UFormField label="Description" name="description">
+          <UInput v-model="state.description" />
+        </UFormField>
 
-      <UFormField label="Category" name="category">
-        <USelect v-model="state.category" :items="CATEGORIES" class="w-48" />
-      </UFormField>
+        <UFormField label="Category" name="category">
+          <USelect v-model="state.category" :items="CATEGORIES" class="w-48" />
+        </UFormField>
 
-      <UButton type="submit" class="inline-flex w-min"> Submit </UButton>
-    </div>
-    <div class="flex flex-col items-center justify-center">
-      <UFormField name="date">
-        <UCalendar v-model="state.date" month-controls :week-starts-on="1" />
-      </UFormField>
-    </div>
-  </UForm>
+        <UButton type="submit" class="inline-flex w-min"> Submit </UButton>
+      </div>
+      <div class="flex flex-col items-center justify-center">
+        <UFormField name="date">
+          <UCalendar v-model="state.date" month-controls :week-starts-on="1" />
+        </UFormField>
+      </div>
+    </UForm>
+  </section>
 </template>
