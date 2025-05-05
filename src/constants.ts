bee51922c827f3583b1f51200cb20a2b542b2c3c
Herob527 <szymon.wrzos@onet.pl>
2025-05-05 16:07:56 +0200

    Added series types

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
+type TransformString<S extends string> = S extends `${infer Head} ${infer Tail}`
+  ? `${TransformString<Head>}-${TransformString<Tail>}`
+  : S;
+
 export const CATEGORIES = (
   [
     'Groceries',
@@ -14,5 +18,14 @@ export const CATEGORIES = (
     'Other',
   ] as const
 ).map((c) => ({ value: c.toLowerCase() as Lowercase<typeof c>, label: c }));
+
+export const SERIES_TYPES = (
+  ['Specific day', 'End of month', 'Start of month'] as const
+).map((type) => ({
+  label: type,
+  value: type.toLowerCase().replace(/\s/g, '-') as TransformString<
+    Lowercase<typeof type>
+  >,
+}));
 
 export type AddMode = 'series' | 'occurrence';
