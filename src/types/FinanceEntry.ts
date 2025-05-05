408bf242af35a7e78588a20f03ff7afd276f38b7
Herob527 <szymon.wrzos@onet.pl>
2025-05-05 16:16:00 +0200

    Added observe to repository

--- src/types/FinanceEntry.ts
+++ src/types/FinanceEntry.ts
@@ -1,3 +1,5 @@
+import type { Observable } from 'rxjs';
+
 export interface FinanceEntry {
   id: number;
   amount: number; // Expense is just negative
@@ -6,10 +8,18 @@ export interface FinanceEntry {
   category: string;
   enabled: boolean;
 }
+
+export interface ObservableData {
+  data: FinanceEntry[];
+  isLoading: boolean;
+  error: unknown;
+}
+
 export interface FinanceRepository {
   add(entry: Omit<FinanceEntry, 'id'>): Promise<void>;
   getAll(): Promise<FinanceEntry[]>;
   update(id: number, entry: FinanceEntry): Promise<void>;
   delete(id: number): Promise<void>;
   toggle(id: number): Promise<void>;
+  observe(): Observable<ObservableData>;
 }
